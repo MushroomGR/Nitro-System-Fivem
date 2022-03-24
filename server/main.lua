d48f1f91c2179d458a8c830819ac3451f064ae43
MushroomGR <40548229+MushroomGR@users.noreply.github.com>
2022-03-24 20:52:13 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,9 +1,9 @@
 RegisterNetEvent('nitro:__sync')
-AddEventHandler('nitro:__sync', function (boostEnabled, lastVehicle)
+AddEventHandler('nitro:__sync', function (boostEnabled, purgeEnabled, lastVehicle)
   local source = source
   for _, player in ipairs(GetPlayers()) do
     if player ~= tostring(source) then
-      TriggerClientEvent('nitro:__update', player, source, boostEnabled, lastVehicle)
+      TriggerClientEvent('nitro:__update', player, source, boostEnabled, purgeEnabled, lastVehicle)
     end
   end
 end)
